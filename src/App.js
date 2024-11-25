8dd54472116cd3f3b4cd2f07b3b288a9fc72d962
skytherium <dragoncil@mail.ru>
2024-11-25 13:16:59 +0200

    update4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./Header/Header";
 import BotPanel from "./BotPanel/BotPanel"
+import ClickPanel from  "./ClickPanel/ClickPanel"
 
 
 const tg=window.Telegram.WebApp;
@@ -18,6 +19,7 @@ function App() {
     <div className="App">
     <Header/>
     <button onClick={onClose}>закрыть</button>
+        <ClickPanel/>
         <BotPanel/>
     </div>
   );
---
+++ src/ClickPanel/ClickPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ClickPanel.css';
+
+const ClickPanel = () => {
+    return (
+        <div className="ClickPanel">
+        <button className="TapButton">
+            123
+        </button>
+            
+        </div>
+    );
+};
+
+export default ClickPanel;
